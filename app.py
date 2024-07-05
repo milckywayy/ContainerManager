afe385e4391f0884cb5d8e2e31e7ec364cf42388
milckywayy <dmitriwear@gmail.com>
2024-07-06 01:09:35 +0200

    Adjusted manager to work with other image types

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import logging
 import random
 import string
-from queue import Queue
 from flask_cors import CORS
 from flask import Flask, request, jsonify
 import docker
@@ -16,18 +15,31 @@ client = docker.from_env()
 
 PASSWORD_LENGTH = 14
 DEFAULT_STUDENT_PASSWORD = '12345678'
-AVAILABLE_PORTS = Queue()
-for p in range(10000, 10501):  # 500 ports
-    AVAILABLE_PORTS.put(p)
+
+AVAILABLE_PORTS = [port for port in range(10000, 10501)]
+OCCUPIED_PORTS = {}
 SESSION_PASSWORDS = {}
-VM_EXPIRATION_TIMES = {}
+CONTAINER_EXPIRATION_TIMES = {}
+
+lock = threading.Lock()
+
 EXPIRATION_INTERVAL = 30  # 30 minutes
 CHECK_INTERVAL = 60  # Check every 60 seconds for expired vms
-VM_TIME_EXTENSION = 15  # minutes
-VM_MIN_TIME_TO_ALLOW_EXTENSION = 15  # minutes
+CONTAINER_TIME_EXTENSION = 15  # minutes
+CONTAINER_MIN_TIME_TO_ALLOW_EXTENSION = 15  # minutes
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+TAG_TO_EXPOSED_PORT = {
+    'vm': 22,
+    'ws': 80,
+    'sws': 443
+}
+
+
+def get_container_name(session_id):
+    return f'CTF-{session_id}'
 
 
 def generate_random_password(length=PASSWORD_LENGTH):
@@ -36,87 +48,129 @@ def generate_random_password(length=PASSWORD_LENGTH):
     return random_password
 
 
-def check_expired_vms():
+def image_exists(image_name):
+    try:
+        client.images.get(image_name)
+        return True
+    except errors.ImageNotFound:
+        return False
+
+
+def is_container_created(container_name):
+    try:
+        client.containers.get(container_name)
+        return True
+    except docker.errors.NotFound:
+        return False
+
+
+def get_tag(image_name):
+    return image_name.split(':')[1]
+
+
+def check_expired_containers():
     while True:
-        now = datetime.now()
-        expired_sessions = [session_id for session_id, expire_time in VM_EXPIRATION_TIMES.items() if now >= expire_time]
+        with lock:
+            now = datetime.now()
+            expired_sessions = [session_id for session_id, expire_time in CONTAINER_EXPIRATION_TIMES.items() if now >= expire_time]
 
         for session_id in expired_sessions:
-            logger.info(f"VM with session ID {session_id} has expired and will be removed.")
-            _remove_vm(session_id, f'VM-{session_id}')
+            logger.info(f"Container with session ID {session_id} has expired and will be removed.")
+            _remove_container(session_id, get_container_name(session_id))
 
         time.sleep(CHECK_INTERVAL)
 
 
-@app.route('/make_vm/<image>', methods=['POST'])
-def make_vm(image):
-    if request.method != 'POST':
-        return jsonify({'error': 'Method not allowed'}), 405
-
+@app.route('/make_container/<image>', methods=['POST'])
+def make_container(image):
     if not AVAILABLE_PORTS:
         logger.error("No available ports")
         return jsonify({'error': 'No available ports'}), 500
 
+    if not image_exists(image):
+        return jsonify({'error': 'Image not found'}), 404
+
     data = request.json
-
     session_id = data['session_id']
-    password = generate_random_password()
-    port = AVAILABLE_PORTS.get()
+    name = get_container_name(session_id)
 
-    logger.info(f"Creating VM with session ID {session_id} on port {port}")
+    if is_container_created(name):
+        return jsonify({'error': 'Container already exists'}), 409
+
+    tag = get_tag(image)
+    with lock:
+        port = random.choice(AVAILABLE_PORTS)
+        exposed_port = TAG_TO_EXPOSED_PORT.get(tag)
+        password = generate_random_password()
+        AVAILABLE_PORTS.remove(port)
+        OCCUPIED_PORTS[session_id] = port
+
+    logger.info(f"Creating container with session ID {session_id} on port {port}")
 
     try:
         vm = client.containers.run(
             image,
             detach=True,
-            ports={'22/tcp': port},
-            name=f'VM-{session_id}'
+            ports={f'{exposed_port}/tcp': port},
+            name=name
         )
 
-        vm.exec_run(f"bash -c 'echo \"student:{password}\" | chpasswd'")
+        if tag == 'vm':
+            SESSION_PASSWORDS[session_id] = password
+            vm.exec_run(f"bash -c 'echo \"student:{password}\" | chpasswd'")
 
-        SESSION_PASSWORDS[session_id] = password
-        VM_EXPIRATION_TIMES[session_id] = datetime.now() + timedelta(minutes=EXPIRATION_INTERVAL)
+        with lock:
+            CONTAINER_EXPIRATION_TIMES[session_id] = datetime.now() + timedelta(minutes=EXPIRATION_INTERVAL)
 
     except Exception as e:
-        AVAILABLE_PORTS.put(port)
+        with lock:
+            AVAILABLE_PORTS.append(port)
+            del OCCUPIED_PORTS[session_id]
+
+            if session_id in SESSION_PASSWORDS:
+                del SESSION_PASSWORDS[session_id]
+
+            if session_id in CONTAINER_EXPIRATION_TIMES:
+                del CONTAINER_EXPIRATION_TIMES[session_id]
+
         logger.error(f"Error creating container: {e}")
         return jsonify({'error': str(e)}), 500
 
-    logger.info(f"Container created: VM-{session_id}")
-    return jsonify(status='Container created', port=port, password=password), 200
+    logger.info(f"Container created: {name}")
+    return jsonify(status='Container created'), 200
 
 
-@app.route('/remove_vm', methods=['DELETE'])
-def remove_vm():
-    if request.method != 'DELETE':
-        return jsonify({'error': 'Method not allowed'}), 405
-
+@app.route('/remove_container', methods=['DELETE'])
+def remove_container():
     data = request.json
     session_id = data['session_id']
-    vm_name = f'VM-{session_id}'
 
-    if _remove_vm(session_id, vm_name):
+    container_name = get_container_name(session_id)
+
+    if _remove_container(session_id, container_name):
         return jsonify(status='Container removed'), 200
     else:
         return jsonify({'error': 'Container could not be removed'}), 500
 
 
-def _remove_vm(session_id, vm_name):
+def _remove_container(session_id, container_name):
     try:
-        vm = client.containers.get(vm_name)
-        port = vm.attrs['NetworkSettings']['Ports']['22/tcp'][0]['HostPort']
-        vm.stop()
-        vm.remove()
-        AVAILABLE_PORTS.put(int(port))
+        container = client.containers.get(container_name)
 
-        if session_id in SESSION_PASSWORDS:
-            del SESSION_PASSWORDS[session_id]
+        container.stop()
+        container.remove()
 
-        if session_id in VM_EXPIRATION_TIMES:
-            del VM_EXPIRATION_TIMES[session_id]
+        with lock:
+            AVAILABLE_PORTS.append(OCCUPIED_PORTS[session_id])
+            del OCCUPIED_PORTS[session_id]
 
-        logger.info(f"Container removed: {vm_name}")
+            if session_id in SESSION_PASSWORDS:
+                del SESSION_PASSWORDS[session_id]
+
+            if session_id in CONTAINER_EXPIRATION_TIMES:
+                del CONTAINER_EXPIRATION_TIMES[session_id]
+
+        logger.info(f"Container removed: {container_name}")
 
     except Exception as e:
         logger.error(f"Error removing container: {e}")
@@ -125,26 +179,39 @@ def _remove_vm(session_id, vm_name):
     return True
 
 
-@app.route('/vm_status', methods=['POST'])
-def vm_status():
+@app.route('/container_status', methods=['POST'])
+def container_status():
     if request.method != 'POST':
         return jsonify({'error': 'Method not allowed'}), 405
 
     data = request.json
-
     session_id = data['session_id']
-    vm_name = f'VM-{session_id}'
+
+    container_name = get_container_name(session_id)
 
     try:
-        vm = client.containers.get(vm_name)
-        expiration_time = VM_EXPIRATION_TIMES.get(session_id, 'No expiration time found')
+        container = client.containers.get(container_name)
+
+        tag = get_tag(container.image.tags[0])
+        port = OCCUPIED_PORTS[session_id]
+
+        access_command = None
+        if tag == 'vm':
+            access_command = f'ssh -p {port} student@<server_domain>'
+        elif tag == 'ws':
+            access_command = f'http://<server_domain>:{port}'
+        elif tag == 'sws':
+            access_command = f'https://<server_domain>:{port}'
+
+        expiration_time = CONTAINER_EXPIRATION_TIMES.get(session_id, 'No expiration time found')
         if isinstance(expiration_time, datetime):
             expiration_time = expiration_time.strftime("%Y-%m-%d %H:%M:%S")
         vm_info = {
-            'status': vm.status,
-            'port': vm.attrs['NetworkSettings']['Ports']['22/tcp'][0]['HostPort'],
-            'password': SESSION_PASSWORDS.get(session_id, 'Password not found'),
-            'expiration_time': expiration_time
+            'status': container.status,
+            'port': OCCUPIED_PORTS.get(session_id),
+            'password': SESSION_PASSWORDS.get(session_id),
+            'expiration_time': expiration_time,
+            'access_command': access_command
         }
         return jsonify(vm_info), 200
 
@@ -156,55 +223,52 @@ def vm_status():
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/extend_vm', methods=['POST'])
-def extend_vm():
-    if request.method != 'POST':
-        return jsonify({'error': 'Method not allowed'}), 405
-
-    data = request.json
-
-    session_id = data['session_id']
-
-    if session_id not in VM_EXPIRATION_TIMES:
-        return jsonify({'error': 'VM session not found'}), 404
-
-    current_expiration = VM_EXPIRATION_TIMES[session_id]
-    time_remaining = (current_expiration - datetime.now()).total_seconds() / 60
-
-    if time_remaining < VM_MIN_TIME_TO_ALLOW_EXTENSION:
-        VM_EXPIRATION_TIMES[session_id] = current_expiration + timedelta(minutes=VM_TIME_EXTENSION)
-        logger.info(f"Extended expiration time for VM with session ID {session_id} by {VM_TIME_EXTENSION} minutes.")
-        return jsonify({'status': 'VM expiration extended'}), 200
-    else:
-        logger.info(
-            f"Cannot extend VM expiration for {session_id}: too much time remaining ({time_remaining} minutes).")
-        return jsonify({'error': f'Cannot extend: more than {VM_MIN_TIME_TO_ALLOW_EXTENSION} minutes remaining'}), 403
-
-
-@app.route('/restart_vm', methods=['POST'])
-def restart_vm():
+@app.route('/extend_container', methods=['POST'])
+def extend_container():
     if request.method != 'POST':
         return jsonify({'error': 'Method not allowed'}), 405
 
     data = request.json
     session_id = data['session_id']
-    vm_name = f'VM-{session_id}'
+
+    with lock:
+        if session_id not in CONTAINER_EXPIRATION_TIMES:
+            return jsonify({'error': 'Container session not found'}), 404
+
+        current_expiration = CONTAINER_EXPIRATION_TIMES[session_id]
+        time_remaining = (current_expiration - datetime.now()).total_seconds() / 60
+
+        if time_remaining < CONTAINER_MIN_TIME_TO_ALLOW_EXTENSION:
+            CONTAINER_EXPIRATION_TIMES[session_id] = current_expiration + timedelta(minutes=CONTAINER_TIME_EXTENSION)
+            logger.info(f"Extended expiration time for container with session ID {session_id} by {CONTAINER_TIME_EXTENSION} minutes.")
+            return jsonify({'status': 'Container expiration extended'}), 200
+        else:
+            logger.info(f"Cannot extend container expiration for {session_id}: too much time remaining ({time_remaining} minutes).")
+            return jsonify({'error': f'Cannot extend: more than {CONTAINER_MIN_TIME_TO_ALLOW_EXTENSION} minutes remaining'}), 403
+
+
+@app.route('/restart_container', methods=['POST'])
+def restart_container():
+    if request.method != 'POST':
+        return jsonify({'error': 'Method not allowed'}), 405
+
+    data = request.json
+    session_id = data['session_id']
+
+    container_name = get_container_name(session_id)
 
     try:
-        vm = client.containers.get(vm_name)
-        vm.restart()
-        logger.info(f"Container restarted: {vm_name}")
+        container = client.containers.get(container_name)
+        container.restart()
+        logger.info(f"Container restarted: {container_name}")
         return jsonify(status='Container restarted'), 200
 
     except docker.errors.NotFound:
-        return jsonify({'error': 'VM not found'}), 404
+        return jsonify({'error': 'Container not found'}), 404
 
     except Exception as e:
         logger.error(f"Error restarting container: {e}")
         return jsonify({'error': str(e)}), 500
 
 
-threading.Thread(target=check_expired_vms, daemon=True).start()
-if __name__ == '__main__':
-    # app.run()
-    pass
+threading.Thread(target=check_expired_containers, daemon=True).start()
